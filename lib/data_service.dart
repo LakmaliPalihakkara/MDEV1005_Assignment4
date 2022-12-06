ab572d82f30e6d17e567f01d5080bd3631937585
binduvelivela <bindu.velivela0202@gmail.com>
2022-12-06 04:47:01 -0500

    Added weather api

---
+++ lib/data_service.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:assignment4/models.dart';
+import 'package:http/http.dart' as http;
+
+class DataService {
+  Future<WeatherResponse> getWeather(String city) async {
+
+    final queryParameters = {
+      'q': city,
+      'appid': 'ad2b4b94927acbaf86681cf4cc92875f',
+      'units': 'imperial'
+    };
+
+    final uri = Uri.https(
+        'api.openweathermap.org', '/data/2.5/weather', queryParameters);
+
+    final response = await http.get(uri);
+    final json = jsonDecode(response.body);
+    return WeatherResponse.fromJson(json);
+  }
+}
